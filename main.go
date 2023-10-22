58ce8f32487bd212f694ff086f41fb9cca97805b
Abhisin90 <abhinavsingh0901@gmail.com>
2023-10-22 14:05:49 +0530

    Added retry functionality

--- main.go
+++ main.go
@@ -1,16 +1,14 @@
 package main
 
 import (
+	"math/rand"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
-	"sync"
 	"github.com/gocolly/colly"
+	"time"
 )
-
-var toBeCrawledPages []string
-
 
 // The function "home" parses and executes an HTML template file named "index.html" and writes the
 // output to the http.ResponseWriter.
@@ -28,11 +26,18 @@ func home(w http.ResponseWriter, r *http.Request){
 	}
 }
 
+var UserAgents = []string {
+	"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36 OPR/38.0.2220.41",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
+	"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.59",
+	"Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1",
+}
 // The function `crawling` uses the Colly library in Go to crawl web pages starting from a given URL.
-func crawling(wg *sync.WaitGroup,url string) {	
-	defer wg.Done()
+func crawling(url string) error {	
+	// defer wg.Done()
 	c := colly.NewCollector()
-	c.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
+	c.UserAgent = UserAgents[rand.Int() % len(UserAgents)]
+	fmt.Println(c.UserAgent)
 	c.OnRequest(func(r *colly.Request) { 
 		fmt.Println("Visiting", r.URL) 	
 	}) 
@@ -50,35 +55,55 @@ func crawling(wg *sync.WaitGroup,url string) {
 		
 	if err := c.Visit(url); err != nil { 
 	    fmt.Println("Error on start of crawl: ", err) 
+		return err
 	} 
-	c.Wait() 
+	c.Wait()
+	return nil 
 }
-
 
 // The function `crawlurl` takes in a URL and a flag indicating if the customer is paid or not, and
 // then adds the URL to a list of pages to be crawled, and starts a certain number of goroutines for
 // crawling based on the customer's paid status. 
 func crawlurl(w http.ResponseWriter, r *http.Request){
 	url := r.FormValue("url")
-	flag := r.FormValue("paid")
-	var isPaidCustomer = false
-	if flag == "true" {
-		isPaidCustomer = true
-	}
-	var wg sync.WaitGroup
-	if(isPaidCustomer){
-		for i := 0; i < 5; i++ {
-			wg.Add(1)
-			go crawling(&wg,url)
-		}
+	crawling(url)
+	// flag := r.FormValue("paid")
+    // var wg sync.WaitGroup
+	// isPaidCustomer := false
+	// if flag == "true" {
+	// 	isPaidCustomer = true
+	// }
+	retries := 3
 
-	} else {
-		for i :=0; i < 2; i++ {
-			wg.Add(1)
-			go crawling(&wg,url)
-		}
-	}
-	wg.Wait()
+    // Set the delay between retries.
+    delay := 100 * time.Millisecond
+
+    // Retry the function until it succeeds.
+    for i := 0; i < retries; i++ {
+        err := crawling(url)
+        if err != nil {
+            fmt.Println("Error:", err)
+            time.Sleep(delay)
+            continue
+        }
+
+        // The function succeeded.
+        break
+    }
+
+	// if(isPaidCustomer){
+	// 	for i := 0; i < 5; i++ {
+	// 		wg.Add(1)
+	// 		go crawling(&wg,url)
+	// 	}
+
+	// } else {
+	// 	for i :=0; i < 2; i++ {
+	// 		wg.Add(1)
+	// 		go crawling(&wg,url)
+	// 	}
+	// }
+	// wg.Wait()
 }
 
 // The handler function routes incoming HTTP requests to different 
@@ -96,3 +121,4 @@ func main(){
 	http.HandleFunc("/",handler)
 	http.ListenAndServe(":8080",nil)
 }
+
